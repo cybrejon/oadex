b68cc9a3d4d570fe43fd39de9e0758126ffe7a38
cybrejon <cybrejon.senpai@gmail.com>
2023-06-18 18:57:22 +0800

    removed page transition due to performance issues in mobile

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,10 @@
   import "../rwd.css";
   import Header from '$lib/Header.svelte';
   import { page } from '$app/stores';
-  import autoAnimate from '@formkit/auto-animate';
   $: curr_page = $page.url.pathname;
 </script>
 
-<div use:autoAnimate class="main-container">
+<div class="main-container">
   <Header active_path={curr_page}/>
   <slot />
 </div>
