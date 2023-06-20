3909299f63e5dd0f1ba0f6c0c9b3f58d252724f9
cybrejon <cybrejon.senpai@gmail.com>
2023-06-20 19:52:40 +0800

    fixed chart

--- src/routes/chart/+page.server.js
+++ src/routes/chart/+page.server.js
@@ -4,14 +4,21 @@ export async function load({ url }) {
 
   console.log(url.searchParams.get('mode'));
 
-  const mode = url.searchParams.get('mode');
+  let mode = url.searchParams.get('mode');
   
   const getWrData = async (mode) => {
     let wrdr = await fetch(`http://150.230.58.91:3000/wr?mode=${mode}`);    
     return await wrdr.json();
   }
 
-  const wrData = await getWrData(mode);
+  let wrData;
+
+  if (mode) {
+    wrData = await getWrData(mode);
+  } else {
+    wrData = await getWrData('all');
+    mode = 'all';
+  }
 
   return {
     wrData,
