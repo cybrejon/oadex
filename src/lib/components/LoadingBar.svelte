8000f59a738c1a43eef05d344af6c56fd940d20f
cybrejon <cybrejon.senpai@gmail.com>
2023-09-03 20:47:31 +0800

    loading animation added

---
+++ src/lib/components/LoadingBar.svelte
@@ -0,0 +1,49 @@
+<script>
+    import { isLoading } from "../../routes/store";
+</script>
+
+
+{#if $isLoading}
+  <div class="container">
+    <div class="progressbar">
+    </div>
+  </div>
+{/if}
+
+<style>
+
+  @keyframes progressbarAnimation {
+    0% {
+      width: 0;
+    }
+    10% {
+      width: 40%;
+    }
+    20% {
+      width: 60%;
+    }
+    60% {
+      width: 70%;
+    }
+    100% {
+      width: 90%;
+    }
+  }
+
+  .container {
+    pointer-events: none;
+    z-index: 200;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 2px;
+  }
+
+  .progressbar {
+    height: 100%;
+    width: 90%;
+    animation: progressbarAnimation 3s;
+    background-color: #ccfe05;
+  }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,7 @@
   import favicon from '$lib/assets/favicon.png';
   import Header from '$lib/Header.svelte';
   import Footer from "$lib/components/Footer.svelte";
+  import LoadingBar from "../lib/components/LoadingBar.svelte";
   import { page } from '$app/stores';
   $: curr_page = $page.url.pathname;
   $: routeId = $page.route.id;
@@ -14,12 +15,24 @@
   // vercel analytics
   import { dev } from '$app/environment';
   import { inject } from '@vercel/analytics';
+  import { isLoading } from "./store";
+  import { afterNavigate, beforeNavigate } from '$app/navigation';
   inject({ mode: dev ? 'development' : 'production' });
+
+  beforeNavigate(() => {
+    isLoading.update(bool => bool = true);
+  });
+
+  afterNavigate(() => {
+    isLoading.update(bool => bool = false);
+  });
 </script>
 
 <svelte:head>
   <link rel="icon" href={favicon} />
 </svelte:head>
+
+<LoadingBar />
 
 <div class="main-container">
   <Header
