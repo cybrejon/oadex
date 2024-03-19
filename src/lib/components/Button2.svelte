39afef4a43a90f2d1a4a12cf20c15e00cddf0ca9
cybrejon <cybrejon.senpai@gmail.com>
2024-03-20 04:17:06 +0800

    Add disabled state

--- src/lib/components/Button2.svelte
+++ src/lib/components/Button2.svelte
@@ -4,12 +4,13 @@
   export let fn;
   export let link;
   export let active = false;
+  export let disabled = false;
   export let styles;
   import Icon from '@iconify/svelte';
 </script>
 
 {#if type === 'button'}
-  <button style={styles} class:iconless-padding={!icon} class:active type="button" on:click={fn}>
+  <button style={styles} class:iconless-padding={!icon} class:active type="button" on:click={fn} class:disabled>
     {#if icon}
       <Icon icon={icon} style='font-size: 24px;' />
     {/if}
@@ -18,7 +19,7 @@
 {/if}
 
 {#if type === 'link'}
-  <a style={styles} class:iconless-padding={!icon} class:active href={link} on:click={fn}>
+  <a style={styles} class:iconless-padding={!icon} class:active href={link} on:click={fn} class:disabled>
     {#if icon}
       <Icon icon={icon} style='font-size: 24px;' />
     {/if}
@@ -56,6 +57,12 @@
     background-color: transparent;
     outline-offset: 3px;
     outline: 2px dashed transparent;
+  }
+
+  .disabled {
+    cursor: not-allowed;
+    opacity: .3;
+    pointer-events: none;
   }
 
   a:hover {
