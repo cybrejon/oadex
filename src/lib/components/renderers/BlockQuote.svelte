8dc0ec7fec992d0d6538f3c66dd6306540be4dd3
cybrejon <cybrejon.senpai@gmail.com>
2024-05-16 13:42:25 +0800

    Custom markdown renderers for shikigami guides

---
+++ src/lib/components/renderers/BlockQuote.svelte
@@ -0,0 +1,29 @@
+<script>
+  import Icon from "@iconify/svelte";
+  export let text = "";
+  import SvelteMarkdown from 'svelte-markdown';
+</script>
+
+<div class="blockquote-container">
+  <Icon icon='tabler:bulb-filled' style='font-size: 3rem;' />
+  <SvelteMarkdown source={text} />
+</div>
+
+<style>
+  .blockquote-container {
+    margin: 15px 0;
+    background-color: #3C3F46;
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+    padding: 20px;
+    gap: 20px;
+    border-radius: 6px;
+  }
+
+  :global(.blockquote-container) {
+    & > p {
+      line-height: 26px;
+    }
+  }
+</style>
---
+++ src/lib/components/renderers/List.svelte
@@ -0,0 +1,25 @@
+<script>
+  export let ordered = false;
+  export let start;
+</script>
+
+{#if ordered}
+  <ol {start}>
+    <slot />
+  </ol>
+{:else}
+  <ul>
+    <slot />
+  </ul>
+{/if}
+
+
+<style>
+  ul, ol {
+    padding-left: 25px;
+    margin: 5px 0 15px 0;
+    display: flex;
+    flex-direction: column;
+    gap: 15px;
+  }
+</style>
