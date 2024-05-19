973ef28c32f49aa438da0e179c99f936a808b686
cybrejon <cybrejon.senpai@gmail.com>
2024-05-19 20:23:13 +0800

    Process markdown in SSR instead of client-side

--- src/routes/shikigami/[slug]/guide/[slug]/+page.server.js
+++ src/routes/shikigami/[slug]/guide/[slug]/+page.server.js
@@ -1,11 +1,31 @@
 import { PRIVATE_HOST_IP } from '$env/static/private';
+import remarkParse from 'remark-parse';
+import rehypeSanitize from 'rehype-sanitize'
+import rehypeStringify from 'rehype-stringify'
+import remarkRehype from 'remark-rehype'
+import { unified } from 'unified'
 
 export async function load({ params, url, fetch }) {
   const shikigami_name = url.searchParams.get('shikigami_name');
+  // const raw_guide_data = await fetch(`http://localhost:3001/cms/api/guide?shiki_id=${shikigami_name}&filename=${params.slug}`);
   const raw_guide_data = await fetch(`${PRIVATE_HOST_IP}/cms/api/guide?shiki_id=${shikigami_name}&filename=${params.slug}`);
   const guide_data = await raw_guide_data.json();
 
+  async function parseMarkdown(i) {
+    const file = await unified()
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(rehypeSanitize)
+    .use(rehypeStringify)
+    .process(i);
+
+    return String(file);
+  }
+
+  const markdown = await parseMarkdown(guide_data.md);
+
   return {
-    guide_data
+    guide_data,
+    markdown
   }
 }
