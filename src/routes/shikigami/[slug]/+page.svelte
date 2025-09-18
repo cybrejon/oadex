419db16e144f62c49aaa222b9e9a6f481f58f977
cybrejon <cybrejon.senpai@gmail.com>
2025-09-18 08:26:44 +0800

    docs: remove migration script documentation

--- src/routes/shikigami/[slug]/+page.svelte
+++ src/routes/shikigami/[slug]/+page.svelte
@@ -1,7 +1,3 @@
-<!-- @migration-task Error while migrating Svelte code: `<tr>` cannot be a child of `<table>`. `<table>` only allows these children: `<caption>`, `<colgroup>`, `<tbody>`, `<thead>`, `<tfoot>`, `<style>`, `<script>`, `<template>`. The browser will 'repair' the HTML (by moving, removing, or inserting elements) which breaks Svelte's assumptions about the structure of your components.
-https://svelte.dev/e/node_invalid_placement -->
-<!-- @migration-task Error while migrating Svelte code: `<tr>` cannot be a child of `<table>`. `<table>` only allows these children: `<caption>`, `<colgroup>`, `<tbody>`, `<thead>`, `<tfoot>`, `<style>`, `<script>`, `<template>`. The browser will 'repair' the HTML (by moving, removing, or inserting elements) which breaks Svelte's assumptions about the structure of your components.
-https://svelte.dev/e/node_invalid_placement -->
 <script>
   import { page } from "$app/stores";
   let shiki_id = $page.url.pathname.slice(11);
