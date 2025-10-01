6b41a87430ea561d85fc5b3f389b40f396312513
cybrejon <cybrejon.senpai@gmail.com>
2025-10-01 12:33:37 +0800

    static deployment config

--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,18 @@
-import adapter from '@sveltejs/adapter-vercel';
+import adapter from "@sveltejs/adapter-node";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter(),
-		alias: {
-			$userStore: 'src/userStore',
-			$utils: 'src/utils',
-			$assets: 'src/lib/assets'
-		}
-	},
-	// vitePlugin: {
-	// 	inspector: true
-	// }
+  kit: {
+    adapter: adapter(),
+    alias: {
+      $userStore: "src/userStore",
+      $utils: "src/utils",
+      $assets: "src/lib/assets",
+    },
+  },
+  // vitePlugin: {
+  // 	inspector: true
+  // }
 };
 
 export default config;
