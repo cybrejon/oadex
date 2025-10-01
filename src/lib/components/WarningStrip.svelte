577b2735a60ef30a9fd1815dd1ff78ed4d5d58e9
cybrejon <cybrejon.senpai@gmail.com>
2025-10-02 05:57:16 +0800

    publish warning message

---
+++ src/lib/components/WarningStrip.svelte
@@ -0,0 +1,41 @@
+<script>
+  import Icon from "@iconify/svelte";
+  let is_visible = $state(true);
+</script>
+
+<div class="container" class:visible={is_visible}>
+  <Icon icon="mingcute:alert-line" />
+  <p>
+    OADex is experiencing problems. Please expect broken features and slow
+    performance.
+  </p>
+  <button onclick={() => (is_visible = false)} type="button">Dismiss.</button>
+</div>
+
+<style>
+  .container {
+    display: none;
+    justify-content: center;
+    align-items: center;
+    padding: 2rem 0 1rem;
+    gap: 0.5rem;
+    flex-wrap: wrap;
+    font-size: 0.9rem;
+  }
+
+  .visible {
+    display: flex;
+  }
+
+  button {
+    border: none;
+    background-color: transparent;
+    font-size: inherit;
+    color: #cdfe05;
+    cursor: pointer;
+  }
+
+  button:hover {
+    text-decoration: underline;
+  }
+</style>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,31 +1,31 @@
 <script>
   import "../app.css";
   import "../rwd.css";
-  import favicon from '$lib/assets/favicon.png';
-  import Header from '$lib/Header.svelte';
+  import favicon from "$lib/assets/favicon.png";
+  import Header from "$lib/Header.svelte";
   import Footer from "$lib/components/Footer.svelte";
   import LoadingBar from "$lib/components/LoadingBar.svelte";
-  import { page } from '$app/state';
+  import { page } from "$app/state";
 
   // vercel analytics
-  import { dev } from '$app/environment';
-  import { inject } from '@vercel/analytics';
+  import { dev } from "$app/environment";
+  import { inject } from "@vercel/analytics";
   import { isLoading } from "$userStore/store";
-  import { afterNavigate, beforeNavigate } from '$app/navigation';
+  import { afterNavigate, beforeNavigate } from "$app/navigation";
   import { onMount } from "svelte";
+  import WarningStrip from "../lib/components/WarningStrip.svelte";
   let { data, children } = $props();
-  inject({ mode: dev ? 'development' : 'production' });
+  inject({ mode: dev ? "development" : "production" });
 
   const searchIndex = data.searchIndex;
 
   beforeNavigate(() => {
-    isLoading.update(bool => bool = true);
+    isLoading.update((bool) => (bool = true));
   });
 
   afterNavigate(() => {
-    isLoading.update(bool => bool = false);
+    isLoading.update((bool) => (bool = false));
   });
-
 
   let curr_page = $state(page.url.pathname);
   let routeId = $state(page.route.id);
@@ -35,14 +35,11 @@
   <link rel="icon" href={favicon} />
 </svelte:head>
 
+<WarningStrip />
 <LoadingBar />
 
 <div class="main-container">
-  <Header
-    active_path={curr_page}
-    {routeId}
-    {searchIndex}
-  />
+  <Header active_path={curr_page} {routeId} {searchIndex} />
   {@render children?.()}
   <Footer />
 </div>
