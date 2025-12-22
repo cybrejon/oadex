b42c2ae7e745526025b563c1713f65f65a30e644
cybrejon <cybrejon.senpai@gmail.com>
2025-12-22 19:15:30 +0800

    fix winrate page

--- src/routes/chart/+page.svelte
+++ src/routes/chart/+page.svelte
@@ -1,20 +1,15 @@
 <script>
-  
+  let { data } = $props();
+
   let wrData = data.wrData;
   let images = data.images;
   let mode = data.mode;
 
-  import WinRates from '$lib/components/shikigami/WinRates.svelte';
-  let { data } = $props();
-
+  import WinRates from "$lib/components/shikigami/WinRates.svelte";
 </script>
 
 <svelte:head>
-	<title>OADex | Chart</title>
+  <title>OADex | Chart</title>
 </svelte:head>
 
-<WinRates
-  wdata={wrData}
-  {images}
-  {mode}
-/>
+<WinRates wdata={wrData} {images} {mode} />
